671a0c0b05630d4d12642ea7b9e5273592cdbfed
563954901@qq.com <563954901@qq.com>
2019-06-18 12:47:18 +0800

    三频特殊

--- ConsoleApp1/Class1.cs
+++ ConsoleApp1/Class1.cs
@@ -7,8 +7,8 @@ public class Class1
     {
         Random random = new Random();
         DateTime startdate = new DateTime(2018,6,1);
-        int m = 6,year=2018;
-            for (int i = 1; i <= 30; i++)
+        int m = 6,year=2019;
+            for (int i = 1; i <= 17; i++)
             {
                 for (int j = 0; j < 3; j++)
                 {
@@ -25,7 +25,7 @@ public class Class1
                                 info.Remark = "一频（昨日17:00-7:00）";
                                 if (i == 1)
                                 {
-                                    info.StartDate = new DateTime(year, m - 1, 31, 17, 00, 00);
+                                    info.StartDate = new DateTime(year, m-1, 31, 17, 00, 00);
                                     info.EndDate = new DateTime(year, m, i, 7, 00, 00);
                                     info.CreateDate = info.EndDate; break;
                                 }
@@ -36,7 +36,7 @@ public class Class1
                             case 2:
                                 info.Remark = "三频（13:01-16:59）";
                                 info.StartDate = new DateTime(year, m, i, 13, 01, 00); info.EndDate = new DateTime(year, m, i, 16, 59, 00); info.CreateDate = info.EndDate;
-                                info.Volume = Convert.ToDecimal(random.Next(10, 17) + random.NextDouble());
+                                info.Volume = Convert.ToDecimal(random.Next(1, 5) + random.NextDouble());
                                 info.LoadingRate = Convert.ToInt32(Convert.ToDouble(info.Volume) * 100 / 17.1510); break;
                         }
                         db.Records.Add(info);
